e69d591deed81a86ac5d1edbbe881adf1c174854
wellsbunk5 <71154493+wellsbunk5@users.noreply.github.com>
2021-02-03 19:17:47 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Home View
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Take them to the Podcast page
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            // Get method, just show them the add movie page
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(Movie movie)
+        {
+            //post method
+            // if the model is valid, add it to the list and then take them to the home page
+            if (ModelState.IsValid)
+            {
+                Data.AddMovie(movie);
+                return View("Index");
+            }
+            else
+            {
+                // if it isn't valid keep them on the page
+                return View();
+            }  
+        }
+
+        public IActionResult MovieList()
+        {
+            // take them to the Movie list page and pass the list of movies but only if
+            // the movies name is not Independence Day
+            return View(Data.Movies.Where(x => x.Title != "Independence Day"));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class Movie
+    {
+
+        // The class with getters and setters for each property
+        // they are all required except the last 3
+        // Notes can't be longer than 25 characters
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public bool? Edited { get; set; }
+        public string LentTo { get; set; }
+        [StringLength(maximumLength:25, ErrorMessage ="Notes cannot exceed 25 characters")]
+        public string Notes { get; set; }
+
+    }
+}
